10e1ddeed5431cfa65d64c71207bfd2c822bb877
Rinaldi Segecin <rsegecin@hotmail.com>
2023-03-09 17:29:36 -0300

    removed unnecessary import

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-import 'package:flutter/services.dart';
 import 'package:browser_image_compression/browser_image_compression.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:intl/intl.dart';
